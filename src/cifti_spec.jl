f5e7566920714474558a4ba7d69f61c08f4cb445
Michael Myers <michael.myers@wustl.edu>
2023-09-15 20:07:30 -0500

    Added files

---
+++ src/cifti_spec.jl
@@ -0,0 +1,57 @@
+
+# from discourse.julialang.org/t/export-enum/5396
+macro exported_enum(name, args...)
+    esc(quote
+        @enum($name, $(args...))
+        export $name
+        $([:(export $arg) for arg in args]...)
+        end)
+    end
+export exported_enum
+
+@exported_enum(BrainStructure,
+    CORTEX_LEFT, CORTEX_RIGHT,
+    ACCUMBENS_LEFT, ACCUMBENS_RIGHT,
+    AMYGDALA_LEFT, AMYGDALA_RIGHT,
+    BRAIN_STEM,
+    CAUDATE_LEFT, CAUDATE_RIGHT,
+    CEREBELLUM_LEFT, CEREBELLUM_RIGHT,
+    DIENCEPHALON_VENTRAL_LEFT, DIENCEPHALON_VENTRAL_RIGHT,
+    HIPPOCAMPUS_LEFT, HIPPOCAMPUS_RIGHT,
+    PALLIDUM_LEFT, PALLIDUM_RIGHT,
+    PUTAMEN_LEFT, PUTAMEN_RIGHT,
+    THALAMUS_LEFT, THALAMUS_RIGHT,
+    # in my experience, structures in cifti files are limited to the above;
+    # but the specification lists the following additional possible values:
+    CORTEX,
+    CEREBELLUM,
+    CEREBELLAR_WHITE_MATTER_LEFT, CEREBELLAR_WHITE_MATTER_RIGHT,
+    OTHER_WHITE_MATTER, OTHER_GREY_MATTER,
+    ALL_WHITE_MATTER, ALL_GREY_MATTER,
+    OTHER
+)
+export BrainStructure
+
+const nifti_hdr_size = 540
+
+# we refer to left and right cortex so often that it's worth having a shorthand
+const L = CORTEX_LEFT
+const R = CORTEX_RIGHT
+export L, R
+
+# dict to map integer codes representing data type found in the nifti2 header
+# (note: not listing certain of those codes that aren't primitive Julia types)
+const dtypes = Dict{Int16, DataType}(
+    1 => Bool,
+    2 => UInt8,
+    4 => Int16,
+    8 => Int32,
+    16 => Float32,
+    64 => Float64,
+    256 => Int8,
+    512 => UInt16,
+    768 => UInt32,
+    1024 => Int64,
+    1280 => UInt64
+)
+
---
+++ src/io.jl
@@ -0,0 +1,109 @@
+
+# the below assumes we'll deal with headers of the NIfTI-2 spec;
+# there are many more fields available, but this is sufficient for basic use
+struct NiftiHeader
+    dtype::DataType
+    nrows::Int64
+    ncols::Int64
+    vox_offset::Int64
+end
+
+struct CiftiStruct
+    hdr::NiftiHeader
+	 data::Matrix
+    brainstructure::OrderedCollections.OrderedDict{BrainStructure, UnitRange}
+    function CiftiStruct(hdr, data, brainstructure)
+        dims = size(data)
+        @assert(hdr.nrows == dims[2], "Expected $(hdr.nrows) rows, found $(dims[2])")
+        @assert(hdr.ncols == dims[1], "Expected $(hdr.ncols) columns, found $(dims[1])")
+        if length(brainstructure) > 0
+            brainstruct_max = brainstructure[collect(keys(brainstructure))[end]][end]
+            @assert(
+                brainstruct_max in dims,
+                "Max index of brainstructure should match data's spatial dimension size"
+            )
+        end
+        new(hdr, data, brainstructure)
+    end
+end
+
+function get_nifti2_hdr(fid::IOStream)::NiftiHeader
+    seek(fid, 0)
+    bytes = zeros(UInt8, nifti_hdr_size)
+    readbytes!(fid, bytes, nifti_hdr_size)
+    test = reinterpret(Int16, bytes[1:2])[1]
+    @assert(test == nifti_hdr_size, "File doesn't seem to follow the NIfTI-2 specs!")
+    dtype = dtypes[reinterpret(Int16, bytes[13:14])[1]]
+    dims = reinterpret(Int64, bytes[17:80])
+    nrows = dims[6]
+    ncols = dims[7]
+    vox_offset = reinterpret(Int64, bytes[169:176])[1]
+    NiftiHeader(dtype, nrows, ncols, vox_offset)
+end
+
+function get_cifti_data(fid::IOStream, hdr::NiftiHeader)
+    seek(fid, hdr.vox_offset)
+    bytes_to_read = hdr.nrows * hdr.ncols * sizeof(hdr.dtype)
+    data = zeros(UInt8, bytes_to_read)
+    readbytes!(fid, data, bytes_to_read)
+    @chain data begin
+		 reinterpret(hdr.dtype, _) 
+		 reshape(_, (hdr.nrows, hdr.ncols)) 
+		 transpose
+	 end
+end
+
+function extract_xml(fid::IOStream, hdr::NiftiHeader)::EzXML.Node
+    # parse xml from raw bytes that follow the hdr
+    seek(fid, nifti_hdr_size)
+    bytes = zeros(UInt8, hdr.vox_offset - nifti_hdr_size)
+    readbytes!(fid, bytes, hdr.vox_offset - nifti_hdr_size)
+    filter!(.!iszero, bytes) # the below will error if we don't remove null bytes
+    start_at = 1 + findfirst(bytes .== UInt8('\n')) # xml begins after 1st newline
+    Chain.@chain begin
+        bytes[start_at:end] 
+        Char.(_) 
+        join 
+        EzXML.parsexml 
+        EzXML.root
+    end
+end
+
+function parse_brainmodel(
+        docroot::EzXML.Node)::OrderedCollections.OrderedDict{BrainStructure, UnitRange}
+    brainmodel_nodes = findall("//BrainModel", docroot)
+    brainstructure = OrderedDict{BrainStructure, UnitRange}()
+    for node in brainmodel_nodes
+        struct_name = 
+			  @chain node["BrainStructure"] begin
+                replace(_, r"CIFTI_STRUCTURE_" => "")
+                Meta.parse
+                eval
+            end
+        start = parse(Int, node["IndexOffset"]) + 1
+        stop = start + parse(Int, node["IndexCount"]) - 1
+        brainstructure[struct_name] = start:stop
+    end
+    brainstructure
+end
+
+get_brainstructure(fid::IOStream, hdr::NiftiHeader) = 
+    extract_xml(fid, hdr) |> parse_brainmodel
+
+"""
+    load(filename)
+
+Read a CIFTI file. Returns a CiftiStruct, a struct composed of the data matrix `data`,
+a rudimentary header `hdr`, and a dictionary of anatomical indices `brainstructure`
+for indexing into the data
+"""
+function load(filename::String)::CiftiStruct
+    @assert(isfile(filename), "$filename doesn't exist")
+    open(filename, "r") do fid
+        hdr = get_nifti2_hdr(fid)
+        data = get_cifti_data(fid, hdr)
+        brainstructure = get_brainstructure(fid, hdr)
+		  CiftiStruct(hdr, data, brainstructure)
+    end
+end
+
