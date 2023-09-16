c5ab37dc6055b010a7f7af54fb2f3b6cccc5bc52
Michael Myers <michael.myers@wustl.edu>
2023-09-15 21:59:00 -0500

    Added brainstructure equivalence test

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,17 +10,37 @@ filetypes = ["dscalar", "dtseries", "pconn", "ptseries"]
 (https://www.fieldtriptoolbox.org/) from the same cifti files as are contained
 here in the `./data/` folder. Files read in via *this* Julia package however
 will differ slightly from "ground truth" because we are reading cifti files in
-their native format (Float32) vs the 64-bit double-precision floats used in MATLAB
+their native format (Float32) vs the 64-bit double-precision used in MATLAB
 """
 ground_truth = load(joinpath(data_dir, "fieldtrip_objects.jld"))
 tol = 1e-6
+
+function test_brainstructure(a::CIFTI.CiftiStruct, b::Dict)
+	if haskey(b, "brainstructure")
+		vals = filter(x -> x > 0, Int.(b["brainstructure"]))
+		ks = b["brainstructurelabel"][:]
+		length(ks) == length(a.brainstructure) || return false
+		temp = Dict([ks[i] => findall(vals .== i) for i in 1:length(ks)])
+		for k in ks
+			rng_a = a.brainstructure[eval(Meta.parse(k))]
+			rng_b = temp[k][1]:temp[k][end]
+			rng_a == rng_b || return false
+		end
+	else
+		length(a.brainstructure) == 0 || return false
+	end
+	return true
+end
 
 @testset "CIFTI.jl" begin
     for filetype in filetypes
         a = CIFTI.load(joinpath(data_dir, "test.$filetype.nii"))
         b = ground_truth["$(filetype)_test"]
-        inds = findall(isfinite.(b["data"]))
-        @test maximum(abs.(a.data[inds] .- b["data"][inds])) < tol
+		  @test test_brainstructure(a, b)
+		  inds_a = findall(isfinite.(a.data))
+        inds_b = findall(isfinite.(b["data"]))
+		  @test inds_a == inds_b
+        @test maximum(abs.(a.data[inds_a] .- b["data"][inds_b])) < tol
     end
 end
 
